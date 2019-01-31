6589a09d63ef3d0103fd89e1598c669df934240e
Andrey Efremov <duxus@yandex.ru>
2019-01-31 13:23:19 +0700

    Update license

--- src/wavreader.cpp
+++ src/wavreader.cpp
@@ -1,3 +1,21 @@
+/*
+ * This file is part of TFA.
+ * Copyright (C) 2013-2019  Andrey Efremov <duxus@yandex.ru>
+ *
+ * TFA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TFA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TFA.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include "wavreader.h"
 #include <QMessageBox>
 #include <QFile>
