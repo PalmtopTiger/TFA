550b66a45f1e65fe32fe2128200e450b7815cf6e
Andrey Efremov <duxus@yandex.ru>
2023-06-05 11:14:03 +0700

    Properly set style

--- src/main.cpp
+++ src/main.cpp
@@ -22,12 +22,13 @@
 
 int main(int argc, char *argv[])
 {
+    QApplication::setStyle("Fusion");
+
     QApplication a(argc, argv);
     a.setApplicationName("TFA");
     a.setApplicationVersion("1.0");
     a.setOrganizationName("Unlimited Web Works");
 	a.setWindowIcon(QIcon(":/main.ico"));
-    a.setStyle("Fusion");
 
     MainWindow w;
     w.show();
